d3248f66eb33fc3cb2bf7b3c3854c1c364ec802a
Ropiudin Ropiudin <ropiudin@dkatalis.com>
2024-08-11 10:22:06 +0700

    chore : add more key

--- lib/models/detail_surat.dart
+++ lib/models/detail_surat.dart
@@ -2,32 +2,96 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'detail_surat.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class DetailSurat {
+  final int nomor;
+  final String nama;
   final String namaLatin;
+  final int jumlahAyat;
+  final String tempatTurun;
   final String arti;
   final String deskripsi;
+  final AudioFull audioFull;
   final List<Ayat> ayat;
 
   DetailSurat({
+    required this.nomor,
+    required this.nama,
     required this.namaLatin,
+    required this.jumlahAyat,
+    required this.tempatTurun,
     required this.arti,
     required this.deskripsi,
+    required this.audioFull,
     required this.ayat,
   });
 
   factory DetailSurat.fromJson(Map<String, dynamic> json) =>
       _$DetailSuratFromJson(json);
+
   Map<String, dynamic> toJson() => _$DetailSuratToJson(this);
 }
 
 @JsonSerializable()
+class AudioFull {
+  final String? audio01;
+  final String? audio02;
+  final String? audio03;
+  final String? audio04;
+  final String? audio05;
+
+  AudioFull({
+    this.audio01,
+    this.audio02,
+    this.audio03,
+    this.audio04,
+    this.audio05,
+  });
+
+  factory AudioFull.fromJson(Map<String, dynamic> json) =>
+      _$AudioFullFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AudioFullToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
 class Ayat {
   final int nomorAyat;
+  final String teksArab;
+  final String teksLatin;
   final String teksIndonesia;
+  final Audio audio;
 
-  Ayat({required this.nomorAyat, required this.teksIndonesia});
+  Ayat({
+    required this.nomorAyat,
+    required this.teksArab,
+    required this.teksLatin,
+    required this.teksIndonesia,
+    required this.audio,
+  });
 
   factory Ayat.fromJson(Map<String, dynamic> json) => _$AyatFromJson(json);
+
   Map<String, dynamic> toJson() => _$AyatToJson(this);
+}
+
+@JsonSerializable()
+class Audio {
+  final String? audio01;
+  final String? audio02;
+  final String? audio03;
+  final String? audio04;
+  final String? audio05;
+
+  Audio({
+    this.audio01,
+    this.audio02,
+    this.audio03,
+    this.audio04,
+    this.audio05,
+  });
+
+  factory Audio.fromJson(Map<String, dynamic> json) => _$AudioFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AudioToJson(this);
 }
--- lib/models/detail_surat.g.dart
+++ lib/models/detail_surat.g.dart
@@ -7,9 +7,14 @@ part of 'detail_surat.dart';
 // **************************************************************************
 
 DetailSurat _$DetailSuratFromJson(Map<String, dynamic> json) => DetailSurat(
+      nomor: (json['nomor'] as num).toInt(),
+      nama: json['nama'] as String,
       namaLatin: json['namaLatin'] as String,
+      jumlahAyat: (json['jumlahAyat'] as num).toInt(),
+      tempatTurun: json['tempatTurun'] as String,
       arti: json['arti'] as String,
       deskripsi: json['deskripsi'] as String,
+      audioFull: AudioFull.fromJson(json['audioFull'] as Map<String, dynamic>),
       ayat: (json['ayat'] as List<dynamic>)
           .map((e) => Ayat.fromJson(e as Map<String, dynamic>))
           .toList(),
@@ -17,18 +22,61 @@ DetailSurat _$DetailSuratFromJson(Map<String, dynamic> json) => DetailSurat(
 
 Map<String, dynamic> _$DetailSuratToJson(DetailSurat instance) =>
     <String, dynamic>{
+      'nomor': instance.nomor,
+      'nama': instance.nama,
       'namaLatin': instance.namaLatin,
+      'jumlahAyat': instance.jumlahAyat,
+      'tempatTurun': instance.tempatTurun,
       'arti': instance.arti,
       'deskripsi': instance.deskripsi,
-      'ayat': instance.ayat,
+      'audioFull': instance.audioFull.toJson(),
+      'ayat': instance.ayat.map((e) => e.toJson()).toList(),
+    };
+
+AudioFull _$AudioFullFromJson(Map<String, dynamic> json) => AudioFull(
+      audio01: json['audio01'] as String?,
+      audio02: json['audio02'] as String?,
+      audio03: json['audio03'] as String?,
+      audio04: json['audio04'] as String?,
+      audio05: json['audio05'] as String?,
+    );
+
+Map<String, dynamic> _$AudioFullToJson(AudioFull instance) => <String, dynamic>{
+      'audio01': instance.audio01,
+      'audio02': instance.audio02,
+      'audio03': instance.audio03,
+      'audio04': instance.audio04,
+      'audio05': instance.audio05,
     };
 
 Ayat _$AyatFromJson(Map<String, dynamic> json) => Ayat(
       nomorAyat: (json['nomorAyat'] as num).toInt(),
+      teksArab: json['teksArab'] as String,
+      teksLatin: json['teksLatin'] as String,
       teksIndonesia: json['teksIndonesia'] as String,
+      audio: Audio.fromJson(json['audio'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$AyatToJson(Ayat instance) => <String, dynamic>{
       'nomorAyat': instance.nomorAyat,
+      'teksArab': instance.teksArab,
+      'teksLatin': instance.teksLatin,
       'teksIndonesia': instance.teksIndonesia,
+      'audio': instance.audio.toJson(),
+    };
+
+Audio _$AudioFromJson(Map<String, dynamic> json) => Audio(
+      audio01: json['audio01'] as String?,
+      audio02: json['audio02'] as String?,
+      audio03: json['audio03'] as String?,
+      audio04: json['audio04'] as String?,
+      audio05: json['audio05'] as String?,
+    );
+
+Map<String, dynamic> _$AudioToJson(Audio instance) => <String, dynamic>{
+      'audio01': instance.audio01,
+      'audio02': instance.audio02,
+      'audio03': instance.audio03,
+      'audio04': instance.audio04,
+      'audio05': instance.audio05,
     };
