1762dfe729dcbe9ddd4da35f2840ec99490bf617
Ropiudin Ropiudin <ropiudin@dkatalis.com>
2024-08-14 15:57:09 +0700

    chore : add fingerprint to MainActivity

--- android/app/src/main/kotlin/com/example/quran/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quran/MainActivity.kt
@@ -1,6 +1,61 @@
 package com.example.quran
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.util.concurrent.Executor
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
+    private val CHANNEL = "fingerprint"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "authenticate") {
+                    authenticate(result)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    private fun authenticate(result: MethodChannel.Result) {
+        val biometricManager = BiometricManager.from(this)
+        if (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) != BiometricManager.BIOMETRIC_SUCCESS) {
+            result.success(false)
+            return
+        }
+
+        val executor: Executor = ContextCompat.getMainExecutor(this)
+        val biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(authResult: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(authResult)
+                result.success(true)
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                result.success(false)
+            }
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                result.success(false)
+            }
+        })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Login with Fingerprint")
+            .setSubtitle("Use your fingerprint to authenticate")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
 }
