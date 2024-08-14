aabf51cfef979705db0d8bd512226af5cba325f0
Ropiudin Ropiudin <ropiudin@dkatalis.com>
2024-08-14 15:57:50 +0700

    chore : implement login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quran/pages/login_page.dart';
 import 'pages/surat_list_page.dart';
 import 'pages/surat_detail_page.dart';
 
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const SuratListPage(),
+        '/': (context) => const LoginPage(),
+        '/list-surat': (context) => const SuratListPage(),
         '/detail': (context) {
           final int nomor = ModalRoute.of(context)!.settings.arguments as int;
           return SuratDetailPage(nomor: nomor);
